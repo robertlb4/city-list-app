7abb2ff99d83bee2e79084cf171eab9e53ac3a08
Robert Brown <robertlb4@gmail.com>
2019-04-01 11:58:51 -0700

    create form component

---
+++ src/app/components/city-form/city-form.component.ts
@@ -0,0 +1,34 @@
+import { Component, OnInit, ViewChild, Output, EventEmitter } from '@angular/core';
+import { LocationService } from '../../location.service';
+import { Observable } from 'rxjs';
+
+@Component({
+  selector: 'app-city-form',
+  templateUrl: './city-form.component.html',
+  styleUrls: ['./city-form.component.scss']
+})
+export class CityFormComponent implements OnInit {
+  @ViewChild('form') form;
+  @Output() addCity = new EventEmitter()
+
+
+  filteredStates$: Observable<string[]>;
+
+  constructor(private _location: LocationService) { }
+
+  ngOnInit() {
+    this.filteredStates$ = this._location.StatesAsObs()    
+  }
+
+  filterStates(value) {
+    this._location.updatefilterdStates(value)
+  }
+
+  formSubmit(formInfo) {
+    this.addCity.emit(formInfo)
+    // this.previousEntries.push(cityInfo)
+    this.form.resetForm()
+}
+
+
+}
