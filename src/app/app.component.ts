a347232d7e1bb422c09525f5044832a9581ee607
Robert Brown <robertlb4@gmail.com>
2019-04-01 12:00:47 -0700

    add components to main view

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,46 @@
-import { Component } from '@angular/core';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { LocationService } from './location.service';
+import { Observable } from 'rxjs';
+// import { filter } from 'rxjs/operators';
+import { MatDialog, MatSnackBar } from '@angular/material';
+import { RandomCityModalComponent } from './components/random-city-modal/random-city-modal.component'
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss']
 })
-export class AppComponent {
-  title = 'fortune-builder-app';
+
+export class AppComponent implements OnInit {
+
+  filteredStates$: Observable<string[]>;
+  savedCities: any = [];
+
+  constructor(private _location: LocationService, private dialog: MatDialog, private snackBar: MatSnackBar) {}
+
+  ngOnInit() {
+    this.filteredStates$ = this._location.StatesAsObs()
+  }
+
+  updateSavedCities(city) {
+    this.savedCities = [city].concat(this.savedCities)
+  }
+
+  showRandom() {
+    if (this.savedCities.length > 0) {
+      const randomCity = this.savedCities[Math.floor(Math.random() * this.savedCities.length)]
+      const dialogRef = this.dialog.open(RandomCityModalComponent, {
+        width: '400px',
+        data: {randomCity: randomCity}
+      })
+    } else {
+      this.snackBar.open(`No Cities Avaliable`, `OK`, {
+        duration: 2000,
+        verticalPosition: 'bottom',
+        horizontalPosition: 'center',
+        panelClass: ['warn-snackBar']  
+      });
+    }
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,60 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpClientModule, HttpClientJsonpModule } from '@angular/common/http';
+
+import { 
+  MatCardModule,
+  MatFormFieldModule,
+  MatInputModule,
+  MatSelectModule,
+  MatOptionModule,
+  MatButtonModule,
+  MatListModule,
+  MatToolbarModule,
+  MatAutocompleteModule,
+  MatIconModule,
+  MatDialog,
+  MatDialogModule,
+  MatSnackBarModule,
+} from '@angular/material';
 
 import { AppComponent } from './app.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { RandomCityModalComponent } from './components/random-city-modal/random-city-modal.component';
+import { CityFormComponent } from './components/city-form/city-form.component';
+import { CityListComponent } from './components/city-list/city-list.component';
+import { StateValidatorDirective } from './state.validator.directive';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RandomCityModalComponent,
+    CityFormComponent,
+    CityListComponent,
+    StateValidatorDirective,
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    BrowserAnimationsModule,
+    FormsModule,
+    HttpClientModule,
+    HttpClientJsonpModule,
+    MatCardModule,
+    MatFormFieldModule,
+    MatInputModule,
+    MatSelectModule,
+    MatOptionModule,
+    MatButtonModule,
+    MatListModule,
+    MatToolbarModule,
+    MatAutocompleteModule,
+    MatIconModule,
+    MatDialogModule,
+    MatSnackBarModule,
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [RandomCityModalComponent]
 })
 export class AppModule { }
