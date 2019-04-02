d060ef108d7b7c14f39c79f05fa2f5aac8804292
Robert Brown <robertlb4@gmail.com>
2019-04-02 01:09:31 -0700

    persist saved cities add comments

--- src/app/location.service.ts
+++ src/app/location.service.ts
@@ -7,40 +7,71 @@ import { environment as env } from '../environments/environment'
   providedIn: 'root'
 })
 export class LocationService {
-
-  public states: string[] = [];
-  private filteredStates: BehaviorSubject<string[]> = new BehaviorSubject([])
+  //holds constant reference to states
+  private states: string[] = [];
+  //value of states filtered by input value
+  private filteredStates: BehaviorSubject<string[]> = new BehaviorSubject([]);
+  //previously saved cities
+  private savedCities = new BehaviorSubject([]);
 
   constructor(private http: HttpClient) { 
-    this.getStates()
+    //init app state
+    this.getStates();
+    this.initSavedCities();
   }
-
+//transmit new values based on filter results
   updatefilterdStates(value?: string) {
-    let filtered = this.states
+    //default if no input value
+    let filtered = this.states;
     if (value) {
-      filtered = this.states.filter(state => state.toLowerCase().startsWith(value.toLowerCase()))
+      filtered = this.states.filter(state => state.toLowerCase().startsWith(value.toLowerCase()));
     }
-    this.filteredStates.next(filtered)
+    this.filteredStates.next(filtered);
   }
-
+  //return subject as observable for subscription on components
   StatesAsObs() {
-    return this.filteredStates.asObservable()
+    return this.filteredStates.asObservable();
   }
 
+  //get states form 3rd party api
   getStates() {
     const URL: string = 'http://battuta.medunes.net/api/region/us/all/?callback=foo&key=00000000000000000000000000000000';
     this.http.jsonp(URL, 'callback')
       .subscribe((results: any[]) => {
-        this.states = results.map(state => state.region)
+        this.states = results.map(state => state.region.toLowerCase());
         this.updatefilterdStates();
       }, e => {
-        console.log(e)
+        console.log(e);
+      })
+  }
+  //get time and timezone info from bing api
+  getTime(city) {
+    const URL = `https://dev.virtualearth.net/REST/v1/TimeZone/?query=${city.city},${city.state},us&key=${env.BING_API}`;
+    return this.http.get(URL);
+  }
+
+  //retrieve saved cities form backend for initial app loading
+  initSavedCities() {
+    this.http.get(`http://localhost:3000/api/cities`)
+      .subscribe((res: any) => {
+        this.savedCities.next(res);
       })
   }
 
-  getTime(city) {
-    const URL = `https://dev.virtualearth.net/REST/v1/TimeZone/?query=${city.city},${city.state},us&key=${env.BING_API}`
-    return this.http.get(URL)
+  //return subject as observable for subscription on components
+  SavedCitiesAsObs() {
+    return this.savedCities.asObservable()
+  }
+  //transmit new values based on filter results
+  updateSavedCities(city) {
+    this.savedCities.next([...this.savedCities.getValue(), city]);
+  }
+  //save to back end and transmit new values
+  saveCity(city) {
+    this.http.post(`http://localhost:3000/api/cities`, city)
+      .subscribe(res => {
+        this.updateSavedCities(res);
+      })
   }
 
 }
